fc53bbccec7f546b8e984148b6983a3acf48e2f8
nguem <nguemojosue@gmail.com>
2019-10-21 20:37:48 +0200

    swiftdrugs api endpoints structure done

--- react-src/src/services/user.services.js
+++ react-src/src/services/user.services.js
@@ -6,22 +6,21 @@ export const userService = {
 };
 
 function subscribe(user) {
-    console.log('preparing to post!')
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(user)
     };
-
-    return fetch(`${config.apiUrl}/subscribe`, requestOptions).then(handleResponse);
+    // return fetch(`${config.apiUrl}/subscribe`, requestOptions).then(handleResponse);
+    return fetch(`${config.apiUrl}/products`, requestOptions).then(handleResponse);
 }
 
 function handleResponse(response) {
-    console.log(response);
+    
     return response.text().then(text => {
-        
         const data = text && JSON.parse(text);
-        console.log('data: ' + data);
+        
+        console.log('data: ' + JSON.stringify(data));
         if (!response.ok) {
             if (response.status === 401) {
                 // auto logout if 401 response returned from api
