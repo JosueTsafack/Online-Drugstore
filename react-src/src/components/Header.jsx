db6918bc2b4db3e61bfcb122c8f408044bd7960e
nguem <nguemojosue@gmail.com>
2019-11-05 00:00:34 +0100

    top header links arranged, and styled

--- react-src/src/components/Header.jsx
+++ react-src/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from React from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { appColor, headerHeight } from 'modules/theme';
