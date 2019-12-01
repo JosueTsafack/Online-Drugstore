0d4730b7d893b245e09ba8bb97df33c90d6904d0
nguem <nguemojosue@gmail.com>
2019-12-01 11:29:24 +0100

    little fix

--- react-src/src/containers/PharmacyForm.jsx
+++ react-src/src/containers/PharmacyForm.jsx
@@ -190,8 +190,8 @@ export class PharmacyForm extends React.Component {
           console.log(response);
           this.setState({ userRegisteringResponse: response.message });
         })
-        .catch(error => 
-          alert(`Désolé! Une érreur s'est produite, Veuillez réessayer plutard.`)
+        .catch(
+          error => alert(`Désolé! Une érreur s'est produite, Veuillez réessayer plutard.`),
           // alert(`Désolé! Une érreur s'est produite : ${error}`)
         );
     }
@@ -217,8 +217,8 @@ export class PharmacyForm extends React.Component {
           // console.log(response);
           this.setState({ pharmacyRegisteringResponse: response.message });
         })
-        .catch(error => 
-          alert(`Désolé! Une érreur s'est produite, Veuillez réessayer plutard.`)
+        .catch(
+          error => alert(`Désolé! Une érreur s'est produite, Veuillez réessayer plutard.`),
           // alert(`Désolé! Une érreur s'est produite : ${error}`)
         );
     }
@@ -409,12 +409,12 @@ export class PharmacyForm extends React.Component {
               <button className="btn btn-primary" disabled={!this.state.pharmacyFormValid}>
                 S'abonner
               </button>
-              {registering && (
+              {/* {registering && (
                 <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
               )}
               <Link href="/login" to="/login" className="btn btn-link">
                 Annuler
-              </Link>
+              </Link> */}
             </div>
           </form>
         </div>
@@ -503,12 +503,12 @@ export class PharmacyForm extends React.Component {
               <button className="btn btn-primary" disabled={!this.state.userFormValid}>
                 S'abonner
               </button>
-              {registering && (
+              {/* {registering && (
                 <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
               )}
               <Link href="/login" to="/login" className="btn btn-link">
                 Annuler
-              </Link>
+              </Link> */}
             </div>
           </form>
         </div>
